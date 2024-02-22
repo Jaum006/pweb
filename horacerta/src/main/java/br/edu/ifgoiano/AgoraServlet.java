fa34a6abf13572cd609ac451620620f0bbce21dd
2022108102540079 <2022108102540079@lab02-22.ifgoiano.local>
2024-02-22 15:23:26 -0300

    Desafio concluído!

--- horacerta/src/main/java/br/edu/ifgoiano/AgoraServlet.java
+++ horacerta/src/main/java/br/edu/ifgoiano/AgoraServlet.java
@@ -1,6 +1,10 @@
 package br.edu.ifgoiano;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Date;
 
 import javax.servlet.ServletException;
@@ -14,9 +18,12 @@ public class AgoraServlet extends HttpServlet{
 	
 	@Override
 	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		Date data = new Date();
+		LocalDateTime data = LocalDateTime.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+		String dataFormatada = data.format(formatter);
 		
-		String html = "<html><body><h1>" + data.toString() + "</h1></body></html>";
+		String html = "<html><body><h1>" + dataFormatada + "</h1></body></html>";
 		
 		resp.setContentType("text/html;charset=UTF-8");
 		resp.getWriter().print(html);
